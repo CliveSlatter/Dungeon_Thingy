1c458a1234488119f2d1c349506dc5e58d7d8196
00172 <PR.56Ome69th.40EUS>
2019-07-10 14:05:55 +0100

    Map now shows

--- resources/js/dungeon.js
+++ resources/js/dungeon.js
@@ -1,31 +1,225 @@
-function pageLoad() {
-    checkSettings();
+//
+// Asset loader
+//
+
+let Loader = {
+    images: {}
+};
+
+Loader.loadImage = function (key, src) {
+    let img = new Image();
+
+    let d = new Promise(function (resolve, reject) {
+        img.onload = function () {
+            this.images[key] = img;
+            resolve(img);
+        }.bind(this);
+
+        img.onerror = function () {
+            reject('Could not load image: ' + src);
+        };
+    }.bind(this));
+
+    img.src = src;
+    return d;
+};
+
+Loader.getImage = function (key) {
+    return (key in this.images) ? this.images[key] : null;
+};
+
+//
+// Keyboard handler
+//
+
+let Keyboard = {};
+
+Keyboard.LEFT = 37;
+Keyboard.RIGHT = 39;
+Keyboard.UP = 38;
+Keyboard.DOWN = 40;
+
+Keyboard._keys = {};
+
+Keyboard.listenForEvents = function (keys) {
+    window.addEventListener('keydown', this._onKeyDown.bind(this));
+    window.addEventListener('keyup', this._onKeyUp.bind(this));
+
+    keys.forEach(function (key) {
+        this._keys[key] = false;
+    }.bind(this));
 }
 
-function checkSettings(){
-    let minRoomSizeInput = $('#min-room-size');
-    let maxRoomSizeInput = $('#max-room-size');
-    let roomPlacementAttemptsInput = $('#room-placement-attempts');
-    let extraRoomConnectionsInput = $('#extra-room-connections');
-    alert(minRoomSizeInput + " " + maxRoomSizeInput);
-    minRoomSizeInput.on('input', event => {
-        $('label[for=min-room-size] > .value').text(minRoomSizeInput.val());
-        if (parseInt(maxRoomSizeInput.val()) < parseInt(minRoomSizeInput.val())) {
-            maxRoomSizeInput.val(minRoomSizeInput.val());
-            $('label[for=max-room-size] > .value').text(maxRoomSizeInput.val());
-        }
-    });
-    maxRoomSizeInput.on('input', event => {
-        $('label[for=max-room-size] > .value').text(maxRoomSizeInput.val());
-        if (parseInt(minRoomSizeInput.val()) > parseInt(maxRoomSizeInput.val())) {
-            minRoomSizeInput.val(maxRoomSizeInput.val());
-            $('label[for=min-room-size] > .value').text(minRoomSizeInput.val());
-        }
-    });
-    roomPlacementAttemptsInput.on('input', event => {
-        $('label[for=room-placement-attempts] > .value').text(roomPlacementAttemptsInput.val());
-    });
-    extraRoomConnectionsInput.on('input', event => {
-        $('label[for=extra-room-connections] > .value').text(extraRoomConnectionsInput.val());
-    });
+Keyboard._onKeyDown = function (event) {
+    let keyCode = event.keyCode;
+    if (keyCode in this._keys) {
+        event.preventDefault();
+        this._keys[keyCode] = true;
+    }
+};
+
+Keyboard._onKeyUp = function (event) {
+    let keyCode = event.keyCode;
+    if (keyCode in this._keys) {
+        event.preventDefault();
+        this._keys[keyCode] = false;
+    }
+};
+
+Keyboard.isDown = function (keyCode) {
+    if (!keyCode in this._keys) {
+        throw new Error('Keycode ' + keyCode + ' is not being listened to');
+    }
+    return this._keys[keyCode];
+};
+
+//
+// Game object
+//
+
+let Game = {};
+
+Game.run = function (context) {
+    this.ctx = context;
+    this._previousElapsed = 0;
+
+    let p = this.load();
+    Promise.all(p).then(function (loaded) {
+        this.init();
+        window.requestAnimationFrame(this.tick);
+    }.bind(this));
+};
+
+Game.tick = function (elapsed) {
+    window.requestAnimationFrame(this.tick);
+
+    // clear previous frame
+    this.ctx.clearRect(0, 0, 512, 512);
+
+    // compute delta time in seconds -- also cap it
+    let delta = (elapsed - this._previousElapsed) / 1000.0;
+    delta = Math.min(delta, 0.25); // maximum delta of 250 ms
+    this._previousElapsed = elapsed;
+
+    this.update(delta);
+    this.render();
+}.bind(Game);
+
+// override these methods to create the demo
+Game.init = function () {};
+Game.update = function (delta) {};
+Game.render = function () {};
+
+//
+// start up function
+//
+
+window.onload = function () {
+    alert("Running")
+    let context = document.getElementById('map').getContext('2d');
+    Game.run(context);
+};
+
+
+// map and game stuff
+
+let r, x, y, z;
+for (r = [], x = 0; x < 2; x++)
+    for (r [x] = [], y = 0; y < 4162; y++)
+
+        r [x][y] = Math.ceil(Math.random()*2);
+
+
+let map = {
+    cols: 57,
+    rows: 75,
+    tsize: 64,
+    layers: r,
+    getTile: function (layer, col, row) {
+        return this.layers[layer][row * map.cols + col];
+    }
+};
+
+function Camera(map, width, height) {
+    this.x = 0;
+    this.y = 0;
+    this.width = width;
+    this.height = height;
+    this.maxX = map.cols * map.tsize - width;
+    this.maxY = map.rows * map.tsize - height;
+
+    alert("height: " + this.height + "width: " + this.width + "maxX: " + this.maxX + "maxY " + this.maxY);
 }
+
+Camera.SPEED = 512; // pixels per second
+
+Camera.prototype.move = function (delta, dirx, diry) {
+    // move camera
+    this.x += dirx * Camera.SPEED * delta;
+    this.y += diry * Camera.SPEED * delta;
+    // clamp values
+    this.x = Math.max(0, Math.min(this.x, this.maxX));
+    this.y = Math.max(0, Math.min(this.y, this.maxY));
+};
+
+Game.load = function () {
+    return [
+        Loader.loadImage('tiles', 'assets/dungeonicons.png'),
+    ];
+};
+
+Game.init = function () {
+    Keyboard.listenForEvents(
+        [Keyboard.LEFT, Keyboard.RIGHT, Keyboard.UP, Keyboard.DOWN]);
+    this.tileAtlas = Loader.getImage('tiles');
+    this.camera = new Camera(map, 512, 512);
+};
+
+Game.update = function (delta) {
+    // handle camera movement with arrow keys
+    let dirx = 0;
+    let diry = 0;
+    if (Keyboard.isDown(Keyboard.LEFT)) { dirx = -1; }
+    if (Keyboard.isDown(Keyboard.RIGHT)) { dirx = 1; }
+    if (Keyboard.isDown(Keyboard.UP)) { diry = -1; }
+    if (Keyboard.isDown(Keyboard.DOWN)) { diry = 1; }
+
+    this.camera.move(delta, dirx, diry);
+};
+
+Game._drawLayer = function (layer) {
+    let startCol = Math.floor(this.camera.x / map.tsize);
+    let endCol = startCol + (this.camera.width / map.tsize);
+    let startRow = Math.floor(this.camera.y / map.tsize);
+    let endRow = startRow + (this.camera.height / map.tsize);
+    let offsetX = -this.camera.x + startCol * map.tsize;
+    let offsetY = -this.camera.y + startRow * map.tsize;
+
+    for (let c = startCol; c <= endCol; c++) {
+        for (let r = startRow; r <= endRow; r++) {
+            let tile = map.getTile(layer, c, r);
+            let x = (c - startCol) * map.tsize + offsetX;
+            let y = (r - startRow) * map.tsize + offsetY;
+            if (tile !== 0) { // 0 => empty tile
+                this.ctx.drawImage(
+                    this.tileAtlas, // image
+                    (tile - 1) * map.tsize, // source x
+                    0, // source y
+                    map.tsize, // source width
+                    map.tsize, // source height
+                    Math.round(x),  // target x
+                    Math.round(y), // target y
+                    map.tsize, // target width
+                    map.tsize // target height
+                );
+            }
+        }
+    }
+};
+
+Game.render = function () {
+    // draw map background layer
+    this._drawLayer(0);
+    // draw map top layer
+    this._drawLayer(1);
+};
